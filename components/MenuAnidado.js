3c206a2296cbcc0f7ce45982dfc4d42f36732b50
Luiszeror <luis.robelto@uptc.edu.co>
2026-09-18 19:00:42 -0500

    Snity fix

--- components/MenuAnidado.js
+++ components/MenuAnidado.js
@@ -72,8 +72,8 @@ export default function MenuAnidado({ secciones = [] }) {
                           onClick={() => setPlatoActivo(p)}
                         >
                           <div className="plato-foto">
-                            {p.imagen ? <img src={p.imagen} alt={p.nombre} /> : (p.emoji || '🍽')}
-                          </div>
+  {p.imagenMini ? <img src={p.imagenMini} alt={p.nombre} /> : (p.emoji || '🍽')}
+</div>
                           <div className="plato-info">
                             <h4>{p.nombre}</h4>
                             {p.desc && <p className="desc">{p.desc}</p>}
